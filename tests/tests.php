51de38dbc1c4f5c1d33c5132e8070d534e485633
Neooblaster <neo-blaster@hotmail.fr>
2017-05-23 10:03:34 +0000

    Update CodeCov report

--- tests/tests.php
+++ tests/tests.php
@@ -1,35 +1,17 @@
 <?php
-/**
- * tests.php
- *
- * %DESC BLOCK%
- *
- * @author Nicolas DUPRE
- * @release 20/05/2017
- * @version 1.0.0
- * @package Index
- */
-
-/**
- * Version : 1.0.0 - Release : 20/05/2017 - Author : Nicolas DUPRE
- *
- * ### Version 1.0.0 :: xx.xx.2017
- *
- * • Première Release
- *
- *
- * ### Description Fonctionnelle
- *
- *
- * ### Détail de la fonction
- *
- * @param void
- *
- * @return void
- */
 class Test extends PHPUnit_Framework_TestCase
 {
-    public function testOnePlusOne() {
-        $this->assertEquals(2,1+1);
+    public function testOne() {
+        $this->assertSame(2, MaFonction());
+        $this->assertSame(2, MaFonction(2));
+    }
+}
+
+
+function MaFonction($arg=null){
+    if(!is_null($arg)){
+        return $arg;
+    } else {
+        return 2;
     }
 }
